ed2f5e2cfd1d56f9da45d866089aab7c75d55ec4
Maxwell Borden <maxwellborden@gmail.com>
2018-10-18 17:04:19 -0700

    Moved solutions to provided racket file and added other files for assignment

--- Racket/hw4/hw4.rkt
+++ Racket/hw4/hw4.rkt
@@ -1,5 +1,11 @@
 #lang racket
-; Question 1
+
+(provide (all-defined-out)) ;; so we can put tests in a second file
+
+;; these definitions are simply for the purpose of being able to run the tests
+;; you MUST replace them with your solutions
+;;
+
 (define (sequence low high stride)
   (if (< low high) (cons low (sequence (+ low stride) high stride)) null))
 ; tests
@@ -7,9 +13,27 @@
 ; (sequence 3 8 3) -> `(3 6)
 ; (sequence 3 2 1) -> `()
 
-; Question 2
 (define (string-append-map xs suffix)
   (map (lambda (element) (string-append element suffix)) xs))
 ; tests
 ; (string-append-map `("Beer" "Soda" "Milk") " is my favourite drink.") -> `("Beer is my favourite drink." "Soda is my favourite drink." "Milk is my favourite drink.") 
+
+
+(define list-nth-mod null)
+
+(define stream-for-n-steps null)
+
+(define funny-number-stream null)
+
+(define cat-then-dog null)
+
+(define stream-add-zero null)
+
+
+(define cycle-lists null)
+
+(define vector-assoc null)
+
+(define cached-assoc null)
+
 
